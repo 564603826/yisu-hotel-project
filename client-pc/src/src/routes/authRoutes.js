f3fc14e916bef7ce2c48ba56f2de0c1f11562dcf
Lijie-yang <2246161025@qq.com>
2026-02-10 20:23:26 +0800

    Create authRoutes.js

---
+++ client-pc/src/src/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../controllers/authController');
+
+// 注册接口
+router.post('/register', register);
+
+// 登录接口
+router.post('/login', login);
+
+module.exports = router;
