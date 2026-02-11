3225aca72ef0a1a4bbee239cfe4710f11bb0c0ae
564603826 <564603826@qq.com>
2026-02-11 16:25:14 +0800

    feat(pc): 路由懒加载实现和自定义loader实现路由守卫

--- client-pc/src/router/index.tsx
+++ client-pc/src/router/index.tsx
@@ -1,26 +1,71 @@
-import { createHashRouter } from 'react-router-dom'
-import AuthLayout from '@/layouts/AuthLayout'
-import Login from '@/pages/Login'
-import Register from '@/pages/Register'
-import Home from '@/pages/Home'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
+import React from 'react'
+import LazyLoad from './LazyLoad'
+import { requireGuestLoader, checkRoleLoader } from './loaders'
+import AuthLayout from '@/layouts/AuthLayout' // 登录注册的布局
 
-const router = createHashRouter([
+// 懒加载组件引入
+const Login = React.lazy(() => import('@/pages/Login'))
+const Register = React.lazy(() => import('@/pages/Register'))
+const HomeLayout = React.lazy(() => import('@/layouts/HomeLayout'))
+const MerchantDashboard = React.lazy(() => import('@/pages/Home/Merchant'))
+const AdminDashboard = React.lazy(() => import('@/pages/Home/Admin'))
+const NotFound = React.lazy(() => import('@/pages/404'))
+
+const router = createBrowserRouter([
+  // 1. 公开路由 (无需登录)
   {
-    path: '/home',
-    element: <Home />,
-  },
-  {
+    path: '/',
     element: <AuthLayout />,
+    loader: requireGuestLoader,
+    children: [
+      { path: '', element: <Navigate to="/login" replace /> },
+      { path: 'login', element: <LazyLoad component={Login} /> },
+      { path: 'register', element: <LazyLoad component={Register} /> },
+    ],
+  },
+
+  // 2. 商户端路由 (需要 Merchant 权限)
+  {
+    path: '/merchant',
+    // 整个 /merchant 路径都被 AuthGuard 保护
+    element: <LazyLoad component={HomeLayout} />,
+    loader: checkRoleLoader(['merchant']),
     children: [
       {
-        path: '/',
-        element: <Login />,
+        path: '',
+        element: <Navigate to="dashboard" replace />,
       },
       {
-        path: '/register',
-        element: <Register />,
+        path: 'dashboard',
+        element: <LazyLoad component={MerchantDashboard} />,
+      },
+      // 后续在这里添加更多商户页面，比如：
+      // { path: 'hotels', element: LazyLoad(MyHotels) },
+    ],
+  },
+
+  // 3. 管理员端路由 (需要 Admin 权限)
+  {
+    path: '/admin',
+    element: <LazyLoad component={HomeLayout} />,
+    loader: checkRoleLoader(['admin']),
+    children: [
+      {
+        path: '',
+        element: <Navigate to="dashboard" replace />,
+      },
+      {
+        path: 'dashboard',
+        element: <LazyLoad component={AdminDashboard} />,
       },
     ],
+  },
+
+  // 4. 404
+  {
+    path: '*',
+    element: <LazyLoad component={NotFound} />,
   },
 ])
 
