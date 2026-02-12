54d253b7fe05c6d86c037637cd3555a63edac56f
564603826 <564603826@qq.com>
2026-02-13 01:36:50 +0800

    feat(pc): 新增商户仓库和管理员仓库，商户所需所有方法实现

--- client-pc/src/store/index.ts
+++ client-pc/src/store/index.ts
@@ -2,8 +2,5 @@
 
 // 直接导出所有的 Hooks
 export * from './userStore'
-
-// 💡 架构师提示：
-// 如果你非要一个大对象包裹（虽然不推荐，因为不利于 Tree Shaking），
-// 只能导出纯函数引用，不能在组件外调用 Hook。
-// 但我强烈建议直接使用上面的 export * 写法。
+export * from './merchantStore'
+export * from './adminStore'
